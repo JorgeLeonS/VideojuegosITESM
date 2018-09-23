c94a1a4db657821961fc2d022e8c1aba3ffac336
Jorge <jorgemachu123@gmail.com>
2018-09-22 20:04:39 -0500

    ui-basics ejercicio
    Se subió el ejercico de interfaz de usuario

---
+++ ui-basics/Assets/Scripts/LifeText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeText : MonoBehaviour {
+
+
+    public Text Life;
+
+    public int LifeNum = 10;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate(){
+        UpdateUI();
+    }
+
+    void UpdateUI(){
+        Life.text = "" + LifeNum;
+    }
+}
---
+++ ui-basics/Assets/Scripts/NewShroom.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewShroom : MonoBehaviour {
+
+    public GameObject Spawn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            DoSpawn();
+        }
+	}
+
+    public void DoSpawn(){
+        GameObject go = Instantiate(Spawn, transform.position, Quaternion.identity);
+        go.AddComponent<Rigidbody>();
+        go.AddComponent<BoxCollider>();
+    }
+
+
+}
